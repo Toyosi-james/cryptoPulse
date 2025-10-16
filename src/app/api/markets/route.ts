c471583b5673d74d1d73b9141a6ff80e93b64176
Toyosi-james <Jamestoyosi84@gmail.com>
2025-10-16 18:50:17 +0100

    second commit

--- src/app/api/markets/route.ts
+++ src/app/api/markets/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false",
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
       {
         headers: {
           "Accept": "application/json",
